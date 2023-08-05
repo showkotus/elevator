b0de180f56ed39e015e1fb3c510e39a44bdca85e
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-08-05 22:05:00 +0600

    hospital and freight elavator page done

--- src/subPages/Products/FreightElevator/FreightElevator.jsx
+++ src/subPages/Products/FreightElevator/FreightElevator.jsx
@@ -1,11 +1,74 @@
-import UnderConstruction from "../../../components/UnderConstruction/UnderConstruction";
-
-const FreightElevator = () => {
+import BannerBG from "../../../components/BannerBG/BannerBG";
+import passenger from '../../../assets/banner/lift5.jpeg'
+import { AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai'
+import { useContext, useState } from "react";
+import { NavbarContext } from "../../../context/ContextProvider";
+const FreightElavator = () => {
+    const [showText, setShowText] = useState(false)
+    const { setIsBanner } = useContext(NavbarContext)
+    function changeNav() {
+        setIsBanner(false)
+    }
+    changeNav()
+    const handleText = () => {
+        setShowText(!showText)
+    }
     return (
-        <div>
-            <UnderConstruction/>
+        <div className="marginTop">
+            <BannerBG title={"Freight Elavator"} />
+            <div className="container px-2">
+                <h1 className="my-6 text-2xl font-bold">Freight Elevator</h1>
+                <div className="my-12 flex flex-col md:flex-row items-center gap-[3%]">
+                    <div className="w-full md:w-[42%]">
+                        <img src={passenger} alt="" className="w-full" />
+                    </div>
+                    <div className="w-full md:w-[55%]">
+                        <p className="text-xl text-justify">A freight elevator, also known as a goods or cargo elevator, is a type of industrial elevator specifically designed to transport heavy loads, large items, and goods between different levels of a building or facility. Unlike passenger elevators, which prioritize passenger comfort, freight elevators are built to handle robust and bulky items, making them essential for various industrial, commercial, and manufacturing applications.</p>
+                        <div className="flex gap-x-4 items-center mt-8">
+                            <p className="text-xl">Here are some key features and characteristics of freight elevators:</p>
+                            {
+                                !showText ?
+                                    <button onClick={() => handleText()}><AiFillPlusCircle size={30} /></button>
+                                    :
+                                    <button onClick={() => handleText()}><AiFillMinusCircle size={30} /></button>
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className={`${!showText ? "collapsable-container" : "active_collapsable_container"}`}>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>1. Capacity and Size:</strong> Freight elevators have a significantly higher weight capacity compared to passenger elevators. They can handle loads ranging from a few hundred kilograms to several tons, depending on their design and intended purpose. The elevator&apos;s size is also larger to accommodate large crates, pallets, and equipment.</h3>
+                    </div>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>2. Heavy-Duty Construction:</strong> Freight elevators are constructed with heavy-duty materials, such as reinforced steel, to withstand the rigorous demands of transporting heavy loads. The walls, floors, and doors are designed to endure impacts and resist wear and tear over time.</h3>
+                    </div>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>3. Loading Configurations:</strong> Freight elevators may have different loading configurations, such as front-loading, side-loading, or dual-entry, to allow for easy and efficient loading and unloading of goods.</h3>
+                    </div>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>4. Manual and Automatic Operation:</strong> Some freight elevators require manual operation, where workers operate the elevator controls to move goods between floors. Others are equipped with automatic controls for convenience and efficiency.</h3>
+                    </div>
+                   
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>5. Safety Features:</strong> Freight elevators come with a range of safety features to protect both goods and operators. These features may include door interlocks, emergency stop buttons, overload sensors, and safety gates to prevent accidental access to the elevator shaft.</h3>
+                    </div>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>6. Freight Elevator Doors:</strong> The doors of freight elevators are typically larger and sturdier than those of passenger elevators to accommodate large and bulky items. Some freight elevators have bi-parting doors or collapsible gates for easier loading and unloading.</h3>
+                    </div>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>7. Vertical and Horizontal Transportation:</strong> Freight elevators can be designed for both vertical and horizontal transportation. Some models can move along a horizontal track, allowing goods to be transported to different areas of the building, such as warehouses or loading docks.</h3>
+                    </div>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>8. Industrial Applications:</strong> Freight elevators are widely used in various industrial settings, including manufacturing plants, warehouses, distribution centers, retail stores, and construction sites. They facilitate the movement of heavy machinery, raw materials, finished products, and other goods.</h3>
+                    </div>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>9. Customization:</strong> Manufacturers offer customization options to meet the specific needs of businesses. This includes adjusting the elevator&apos;s size, capacity, and loading configurations to suit the nature of the goods being transported.</h3>
+                    </div>
+                    <p className="text-xl mt-8 text-justify">In summary, freight elevators are essential tools for efficiently and safely moving heavy loads and goods within industrial and commercial settings. They enhance productivity and operational efficiency by streamlining the movement of large and bulky items between different levels of a building or facility.</p>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default FreightElevator; 
+export default FreightElavator;
--- src/subPages/Products/HospitalElevator/HospitalElevator.jsx
+++ src/subPages/Products/HospitalElevator/HospitalElevator.jsx
@@ -1,10 +1,75 @@
-import UnderConstruction from "../../../components/UnderConstruction/UnderConstruction";
-
- 
+import BannerBG from "../../../components/BannerBG/BannerBG";
+import passenger from '../../../assets/banner/lift3.jpg'
+import { AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai'
+import { useContext, useState } from "react";
+import { NavbarContext } from "../../../context/ContextProvider";
 const HospitalElevator = () => {
+    const [showText, setShowText] = useState(false)
+    const { setIsBanner } = useContext(NavbarContext)
+    function changeNav() {
+        setIsBanner(false)
+    }
+    changeNav()
+    const handleText = () => {
+        setShowText(!showText)
+    }
     return (
-        <div>
-            <UnderConstruction/>
+        <div className="marginTop">
+            <BannerBG title={"Hospital Elavator"} />
+            <div className="container px-2">
+                <h1 className="my-6 text-2xl font-bold">Hospital Elevator</h1>
+                <div className="my-12 flex flex-col md:flex-row items-center gap-[3%]">
+                    <div className="w-full md:w-[42%]">
+                        <img src={passenger} alt="" className="w-full" />
+                    </div>
+                    <div className="w-full md:w-[55%]">
+                        <p className="text-xl text-justify">Hospital lifts, also known as hospital elevators or medical elevators, are specialized vertical transportation systems designed to meet the unique needs of healthcare facilities such as hospitals, clinics, and medical centers. These elevators play a crucial role in patient care, allowing for efficient movement of patients, medical staff, and essential medical equipment between different floors of the facility. Hospital lifts are equipped with advanced features and safety measures to ensure the smooth and safe transportation of patients and medical personnel.</p>
+                        <div className="flex gap-x-4 items-center mt-8">
+                            <p className="text-xl">Here&apos;s an elaborate overview of hospital lifts:</p>
+                            {
+                                !showText ?
+                                    <button onClick={() => handleText()}><AiFillPlusCircle size={30} /></button>
+                                    :
+                                    <button onClick={() => handleText()}><AiFillMinusCircle size={30} /></button>
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className={`${!showText ? "collapsable-container" : "active_collapsable_container"}`}>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>1. Patient Transportation:</strong> Hospital lifts are primarily designed to transport patients comfortably and safely. The elevators are spacious enough to accommodate stretchers and medical beds, allowing healthcare professionals to move patients between different hospital departments, such as operating rooms, recovery areas, intensive care units (ICUs), and patient rooms.</h3>
+                    </div>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>2. Bed and Stretcher Compatibility:</strong> Hospital elevators are equipped with larger dimensions and a higher weight capacity to accommodate hospital beds, stretchers, and medical equipment. The design ensures smooth and stable movement to minimize any discomfort for patients during transportation.</h3>
+                    </div>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>3. Specialized Controls:</strong> Hospital elevators often feature user-friendly and specialized controls to facilitate easy access for medical staff. These may include large buttons, hands-free or touchless options, and extra space for medical personnel to operate equipment comfortably while inside the elevator.</h3>
+                    </div>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>4. Emergency Operation:</strong> Hospital lifts are designed with emergency backup systems to ensure operation during power outages or emergencies. This feature is critical to maintaining continuous patient care and the efficient movement of patients during unforeseen events.</h3>
+                    </div>
+                    <div className="my-4">
+                    <h3 className="text-xl"><strong>5. Code Compliance and Safety Features:</strong> Hospital lifts adhere to strict safety regulations and codes specific to healthcare environments. They are equipped with a range of safety features, including:</h3>
+                    <ul className="ml-8 md:ml-20 text-xl list-disc text-justify">
+                        <li>Overload sensors to prevent operation if the elevator exceeds its weight capacity.</li>
+                        <li>Emergency stop buttons to halt the elevator in case of emergencies.</li>
+                        <li>Door sensors to prevent the doors from closing when an object or person is in the doorway.</li>
+                        <li>Fire-rated doors to contain the spread of fire in case of emergencies.</li>
+                        <li>Battery-operated lighting and ventilation to ensure safety during power outages.</li>
+                    </ul>
+                </div>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>6. Infection Control:</strong> Hospitals prioritize infection control, and hospital lifts are no exception. These elevators often feature antimicrobial materials and touchless controls to minimize the risk of cross-contamination and the spread of infections.</h3>
+                    </div>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>7. Priority Service and Destination Dispatch:</strong> Some modern hospital lifts are equipped with destination dispatch systems that optimize elevator traffic flow. This feature helps to reduce waiting times and provides priority service to medical staff transporting patients or critical medical equipment.</h3>
+                    </div>
+                    <div className="my-4">
+                        <h3 className="text-xl"><strong>8. Capacity Management:</strong> Hospital elevators may include systems to monitor and manage elevator usage, allowing staff to prioritize specific floors or maintain specific elevators for certain patient categories.</h3>
+                    </div>
+                    <p className="text-xl mt-8 text-justify">In summary, hospital lifts are specialized vertical transportation systems tailored to the unique requirements of healthcare facilities. They prioritize patient comfort, safety, and efficient movement between floors while also supporting the needs of medical staff and vital medical equipment. Hospital lifts play a crucial role in ensuring effective patient care and streamlined operations within healthcare environments.</p>
+                </div>
+            </div>
         </div>
     );
 };
